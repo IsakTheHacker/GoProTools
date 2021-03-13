06d71359d6c59326facf899c88e8b844c5415356
IsakTheHacker <isak.brynielsson@gmail.com>
2021-03-13 14:39:15 +0100

    Print information about non GoPro items.

--- FolderStatus/folderStatus.py
+++ FolderStatus/folderStatus.py
@@ -1,7 +1,7 @@
 #Imports
 import os
 
-filesInDir = 0
+totalItems = 0
 
 #Arrays
 thmFiles = []
@@ -12,14 +12,21 @@ h264Videos = []
 h265Videos = []
 
 nonGoProFiles = []
+folders = []
 
 def printStats():
 	print("\nContains {} useless files, of which {} are thm files and {} are drv files.".format(len(thmFiles) + len(lrvFiles), len(thmFiles), len(lrvFiles)))
 	print("{} photos (jpg)".format(len(jpgFiles)))
 	print("{} videos (mp4), of which {} use H.264 coding (AVC) and {} use H.265 coding (HEVC)".format(len(h264Videos) + len(h265Videos), len(h264Videos), len(h265Videos)))
+	print("\nNon GoPro stuff:")
+	print("{} files".format(len(nonGoProFiles)))
+	print("{} folders".format(len(folders)))
 
 for entry in os.scandir(os.path.abspath(os.getcwd())):
+	totalItems += 1
+
 	if entry.is_dir():
+		folders.append(entry.name)
 		continue
 		
 	if entry.is_file() and entry.name.lower().endswith(".thm"):
@@ -33,8 +40,8 @@ for entry in os.scandir(os.path.abspath(os.getcwd())):
 			h264Videos.append(entry.name)
 		elif entry.name.lower().startswith("gx"):
 			h265Videos.append(entry.name)
+	else:
+		nonGoProFiles.append(entry.name)
 
-	filesInDir += 1
-
-print("Directory: \"{}\". {} total files!".format(os.path.abspath(os.getcwd()), filesInDir))
+print("Directory: \"{}\". {} total items!".format(os.path.abspath(os.getcwd()), totalItems))
 printStats()
