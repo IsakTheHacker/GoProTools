098e6c2f4027f1485a4df72981ca884319b3836c
IsakTheHacker <isak.brynielsson@gmail.com>
2021-03-20 22:18:54 +0100

    Make the text colored.

--- FolderStatus/folderStatus.py
+++ FolderStatus/folderStatus.py
@@ -1,6 +1,10 @@
 #Imports
 import os
 
+import colorama
+from colorama import Fore
+colorama.init()
+
 totalItems = 0
 
 #Arrays
@@ -15,12 +19,26 @@ nonGoProFiles = []
 folders = []
 
 def printStats():
-	print("\nContains {} useless files, of which {} are thm files and {} are drv files.".format(len(thmFiles) + len(lrvFiles), len(thmFiles), len(lrvFiles)))
-	print("{} photos (jpg)".format(len(jpgFiles)))
-	print("{} videos (mp4), of which {} use H.264 coding (AVC) and {} use H.265 coding (HEVC)".format(len(h264Videos) + len(h265Videos), len(h264Videos), len(h265Videos)))
+	print("\nContains {}{}{} useless files, of which {}{}{} are thm files and {}{}{} are drv files.".format(
+		Fore.BLUE, len(thmFiles) + len(lrvFiles), Fore.RESET,
+		Fore.CYAN, len(thmFiles), Fore.RESET,
+		Fore.CYAN, len(lrvFiles), Fore.RESET
+	))
+	print("{}{}{} photos (jpg)".format(
+		Fore.MAGENTA, len(jpgFiles), Fore.RESET
+	))
+	print("{}{}{} videos (mp4), of which {}{}{} use H.264 coding (AVC) and {}{}{} use H.265 coding (HEVC)".format(
+		Fore.MAGENTA, len(h264Videos) + len(h265Videos), Fore.RESET,
+		Fore.CYAN, len(h264Videos), Fore.RESET,
+		Fore.CYAN, len(h265Videos), Fore.RESET
+	))
 	print("\nNon GoPro stuff:")
-	print("{} files".format(len(nonGoProFiles)))
-	print("{} folders".format(len(folders)))
+	print("{}{}{} files".format(
+		Fore.RED, len(nonGoProFiles), Fore.RESET
+	))
+	print("{}{}{} folders".format(
+		Fore.RED, len(folders), Fore.RESET
+	))
 
 for entry in os.scandir(os.path.abspath(os.getcwd())):
 	totalItems += 1
@@ -43,5 +61,8 @@ for entry in os.scandir(os.path.abspath(os.getcwd())):
 	else:
 		nonGoProFiles.append(entry.name)
 
-print("Directory: \"{}\". {} total items!".format(os.path.abspath(os.getcwd()), totalItems))
+print("Directory: {}\"{}\"{}. {}{}{} total items!".format(
+	Fore.GREEN, os.path.abspath(os.getcwd()), Fore.RESET,
+	Fore.YELLOW, totalItems, Fore.RESET
+))
 printStats()
